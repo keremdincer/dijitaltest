14231b8e4e4a5b3f1c6f227d5cde792788844721
Kerem Dincer <keremdincer@Kerems-MacBook-Pro.local>
2018-08-31 22:18:36 +0300

    New routes added for database reset and test content view.

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,22 @@ const express = require('express')
 const router = express.Router()
 
 const db = require('./../helpers/Database')
+const test = require('./../helpers/Model').test
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  db.createTables()
+router.get('/', async (req, res, next) => {
   res.render('index', { title: 'Express' })
-});
+})
+
+router.get('/test/:id', async (req, res, next) => {
+  let t = await test(req.params.id)  
+  res.render('test', { test: t })
+})
+
+router.get('/wipe', async (req, res, next) => {
+  await db.createTables()
+  await db.populate()
+  res.redirect('/')
+})
 
 module.exports = router;
